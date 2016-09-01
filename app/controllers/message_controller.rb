6bc6e8f24175f0ff661c02c6b86a9184300ee9e2
Cory Johnson <cory.r.johnson@cox.net>
2016-09-01 10:54:15 -0500

    Message will be posted to root page with Name Message and Time feilds. Onto further formatting

--- app/controllers/message_controller.rb
+++ app/controllers/message_controller.rb
@@ -1,6 +1,22 @@
+require 'pry'
+
 class MessageController < ApplicationController
 
 	def index
-		@post = Post.new
+		@post = Post.new #for the form
+
+		@posts = Post.all
+
+		@time = Time.now
 	end
+
+	def create
+		post = Post.new(post_params)
+		post.save
+		redirect_to root_path
+	end
+
+  	def post_params
+      params.require(:post).permit(:name, :message)
+    end
 end
